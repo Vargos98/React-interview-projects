3abb064bc90c3e1584e2e54ef920770736ad75f5
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-08-08 14:04:21 +0530

    Update Accordian.jsx

--- basics/src/components/Accordian/Accordian.jsx
+++ basics/src/components/Accordian/Accordian.jsx
@@ -1,48 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import data from './data';
-// import './styles.css'
-const Accordian = () => {
 
+const Accordion = () => {
   const [selected, setSelected] = useState(null);
   const [enableMultiSelect, setMultiSelect] = useState(false);
   const [multiple, setMultiple] = useState([]);
 
   const handleSingleClick = (id) => {
     setSelected(id === selected ? null : id);
-  }
+  };
 
   const handleMultipleSelection = (id) => {
-    let copyMultiple = [...multiple]
-    const findIndexOfCurrentId = copyMultiple.indexOf(id)
-    if(findIndexOfCurrentId !== -1) { copyMultiple.push(id)}else{ copyMultiple.slice(findIndexOfCurrentId, 1)}
-    setMultiple(copyMultiple)
-  }
+    setMultiple((prev) => 
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
 
   return (
-    <div className='wrapper'>
-      <div className='accordian'>
-      <button onClick={()=>setMultiSelect(!enableMultiSelect)}>Enable Multiple selection</button>
-        {
-          data && data.length > 0 ?
-            data.map(dataItem =>
-              <div onClick={enableMultiSelect ? ()=> handleMultipleSelection(dataItem.id)  :() => handleSingleClick(dataItem.id)} className='title'>
-                <h3>{dataItem.question}</h3>
-                <span>+</span>
-                {
-                  enableMultiSelect ? multiple.indexOf(dataItem.id) !== -1 &&
-                  (<div className="content">{dataItem.answer}</div>)
-               :selected === data.id && (
-                <div className='content'>{dataItem.answer}</div>
-               ) }
-              </div>)
-
-            : <div><h3>Empty data</h3></div>
-        }
-
+    <div className="wrapper">
+      <div className="accordion">
+        <button onClick={() => setMultiSelect(!enableMultiSelect)}>
+          {enableMultiSelect ? "Disable" : "Enable"} Multiple Selection
+        </button>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div
+              key={dataItem.id}
+              onClick={
+                enableMultiSelect
+                  ? () => handleMultipleSelection(dataItem.id)
+                  : () => handleSingleClick(dataItem.id)
+              }
+              className="title"
+            >
+              <h3>{dataItem.question}</h3>
+              <span>+</span>
+              {enableMultiSelect
+                ? multiple.includes(dataItem.id) && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+            </div>
+          ))
+        ) : (
+          <div>
+            <h3>Empty data</h3>
+          </div>
+        )}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Accordian
+export default Accordion;
