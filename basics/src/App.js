0bb586d91247b730039feadd7e5c7d768073d261
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-08-08 12:27:45 +0530

    Completed Random Color generator

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Accordian from './components/Accordian/Accordian'
 
+import './index.css'
+import RandomColor from './components/random-color-generator/RandomColor'
 const App = () => {
   return (
     <div>
-      <Accordian/>
+      <RandomColor/>
     </div>
   )
 }
--- basics/src/components/Accordian/Accordian.jsx
+++ basics/src/components/Accordian/Accordian.jsx
@@ -1,25 +1,39 @@
 import React, { useState } from 'react'
 import data from './data';
+// import './styles.css'
 const Accordian = () => {
 
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelect, setMultiSelect] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   const handleSingleClick = (id) => {
     setSelected(id === selected ? null : id);
   }
 
+  const handleMultipleSelection = (id) => {
+    let copyMultiple = [...multiple]
+    const findIndexOfCurrentId = copyMultiple.indexOf(id)
+    if(findIndexOfCurrentId !== -1) { copyMultiple.push(id)}else{ copyMultiple.slice(findIndexOfCurrentId, 1)}
+    setMultiple(copyMultiple)
+  }
+
   return (
-    <div className='flex items-center justify-center'>
-      <div className='flex items-center justify-center'>
+    <div className='wrapper'>
+      <div className='accordian'>
+      <button onClick={()=>setMultiSelect(!enableMultiSelect)}>Enable Multiple selection</button>
         {
           data && data.length > 0 ?
             data.map(dataItem =>
-              <div onClick={() => handleSingleClick(dataItem.id)} className='title'>
+              <div onClick={enableMultiSelect ? ()=> handleMultipleSelection(dataItem.id)  :() => handleSingleClick(dataItem.id)} className='title'>
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
                 {
-                  selected === dataItem.id ? <div className='content'><h4>{dataItem.answer}</h4></div> : null
-                }
+                  enableMultiSelect ? multiple.indexOf(dataItem.id) !== -1 &&
+                  (<div className="content">{dataItem.answer}</div>)
+               :selected === data.id && (
+                <div className='content'>{dataItem.answer}</div>
+               ) }
               </div>)
 
             : <div><h3>Empty data</h3></div>
